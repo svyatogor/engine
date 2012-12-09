4c741236f315de8df4c02452ebe3d52e9bcbe116
Sergey Kuleshov <svyatogor@gmail.com>
2012-12-09 18:54:40 +0200

    temp notifications fix

--- app/mailers/locomotive/notifications.rb
+++ app/mailers/locomotive/notifications.rb
@@ -4,7 +4,7 @@ module Locomotive
     default :from => Locomotive.config.mailer_sender
 
     def new_content_entry(account, entry)
-      @account, @entry, @type, @domain = account, entry, entry.content_type, entry.site.domains.first
+      @account, @entry, @type, @domain = account, entry, entry.content_type, Locomotive.config
 
       subject = t('locomotive.notifications.new_content_entry.subject', :domain => @domain, :type => @type.name, :locale => account.locale)
 
